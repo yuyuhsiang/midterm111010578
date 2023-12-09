a64ff5f53755a1f33da63d0f2c4a07442b847cfc
yuhsiang <y43023382@gmail.com>
2023-12-09 16:06:48 +0800

    computer system midterm

---
+++ 04/mult/mult.asm
@@ -0,0 +1,31 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/04/Mult.asm
+
+// Multiplies R0 and R1 and stores the result in R2.
+// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+
+// Put your code here.
+
+@2
+M=0
+
+@1
+D=M
+@exit
+D;JLE
+
+@0
+D=M
+@2
+M=M+D
+
+@1
+M=M-1
+
+@loop
+0;JMP
+
+@exit
+0;JMP
